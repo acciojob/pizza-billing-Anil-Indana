d209a638cd8e2dc7d6679bfb378f9bc520ac7477
unknown <you@example.com>
2023-03-18 10:42:53 +0530

    Re-modified

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,11 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
+//    DeluxePizza dp = new DeluxePizza(true);
+//    System.out.println(dp.getPrice());
+//    dp.addExtraCheese();
+//    dp.addTakeaway();
+//    System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
     p.addExtraCheese();
